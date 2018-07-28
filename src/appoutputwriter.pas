7e1759bd1f9c6dc495f83b49b62e318df4245f44
Nikita B. Zuev <nikitazu@gmail.com>
2018-07-28 13:20:08 +0300

    Add GPL headers

--- src/appoutputwriter.pas
+++ src/appoutputwriter.pas
@@ -1,3 +1,21 @@
+{
+  Copyright (C) 2018 Nikita B. Zuev
+
+  This file is part of FFXVPhotoEx.
+
+  FFXVPhotoEx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  FFXVPhotoEx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with FFXVPhotoEx.  If not, see <https://www.gnu.org/licenses/>.
+}
 unit AppOutputWriter;
 
 {$mode objfpc}{$H+}
